ca79a58810e1275ab9ea6cd3b596ae1702d99715
Emilietaklo <emilie.li.taklo@gmail.com>
2026-02-27 14:05:39 +0100

    litt css pirk

--- wordle.js
+++ wordle.js
@@ -72,6 +72,8 @@ document.addEventListener('DOMContentLoaded', () => {
             else{
                 alleRuter[index+1].focus();
             }
+
+            boks.setAttribute("disabled", true);
             
         });
     }
